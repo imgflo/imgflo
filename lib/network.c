imgflo/imgflo

commit 84580eb09955f52d189de328fc1746f12a19661f

runtime: Initial support for interactive preview in Flowhub
Refs #5
Emits a WebSocket message when Processor output is invalidated
with a HTTP /process?node=somenode url.
The UI does a GET request to the url, and gets a PNG image of
the output of the node served on-demand.

lib/network.c
@@ -2,14 +2,25 @@
 //     (c) 2014 The Grid
 //     imgflo may be freely distributed under the MIT license
 
-typedef struct {
+struct _Network;
+
+typedef void (* NetworkProcessorInvalidatedCallback)
+    (struct _Network *network, struct _Processor *processor, GeglRectangle rect, gpointer user_data);
+
+typedef struct _Network {
     Graph *graph; // unowned
+    gboolean running;
+    NetworkProcessorInvalidatedCallback on_processor_invalidated;
+    gpointer on_processor_invalidated_data;
 } Network;
 
 Network *
 network_new(void)
 {
     Network *self = g_new(Network, 1);
+    self->running = FALSE;
+    self->on_processor_invalidated = NULL;
+    self->on_processor_invalidated_data = NULL;
     return self;
 }
 
@@ -20,15 +31,27 @@ network_free(Network *self)
 }
 
 void
+emit_invalidated(Processor *processor, GeglRectangle rect, gpointer user_data) {
+    Network *network = (Network *)user_data;
+    if (network->on_processor_invalidated) {
+        network->on_processor_invalidated(network, processor, rect,
+                                          network->on_processor_invalidated_data);
+    }
+}
+
+
+void
 network_set_graph(Network *self, Graph *graph)
 {
     self->graph = graph;
 }
 
 void 
-set_running_state_func(gpointer key, Processor *value, gboolean *running)
+set_running_state_func(gpointer key, Processor *value, Network *network)
 {
-    processor_set_running(value, *running);
+    processor_set_running(value, network->running);
+    value->on_invalidated_data = (gpointer)network;
+    value->on_invalidated = emit_invalidated;
 }
 
 void
@@ -37,7 +60,7 @@ network_set_running(Network *self, gboolean running)
     if (!self->graph) {
         return;
     }
-    g_hash_table_foreach(self->graph->processor_map, (GHFunc)set_running_state_func, &running);
+    g_hash_table_foreach(self->graph->processor_map, (GHFunc)set_running_state_func, self);
 }
 
 void 
@@ -56,3 +79,4 @@ network_process(Network *self) {
     }
     g_hash_table_foreach(self->graph->processor_map, (GHFunc)process_func, NULL);
 }
+

lib/processor.c
@@ -5,13 +5,20 @@
 #include <glib.h>
 #include <gegl.h>
 
-typedef struct {
+struct _Processor;
+
+typedef void (* ProcessorInvalidatedCallback)
+    (struct _Processor *processor, GeglRectangle rect, gpointer user_data);
+
+typedef struct _Processor {
     gboolean running;
     GeglNode *node;
     guint monitor_id;
     GQueue *processing_queue; /* Queue of rectangles that needs to be processed */
     GeglRectangle *currently_processed_rect;
     GeglProcessor *processor;
+    ProcessorInvalidatedCallback on_invalidated;
+    gpointer on_invalidated_data;
 } Processor;
 
 static gboolean
@@ -27,6 +34,8 @@ processor_new(void) {
     self->processor = NULL;
     self->processing_queue = g_queue_new();
     self->currently_processed_rect = NULL;
+    self->on_invalidated = NULL;
+    self->on_invalidated_data = NULL;
     return self;
 }
 
@@ -70,6 +79,9 @@ invalidated_event(GeglNode *node, GeglRectangle *rect, Processor *self)
 {
     if (self->running) {
         trigger_processing(self, *rect);
+    }
+    if (self->on_invalidated) {
+        self->on_invalidated(self, *rect, self->on_invalidated_data);
     }
 }
 

Makefile
@@ -13,7 +13,7 @@ else
 PKGCONFIG_ARGS:=
 endif
 
-LIBS=gegl-0.3 gio-unix-2.0 json-glib-1.0 libsoup-2.4
+LIBS=gegl-0.3 gio-unix-2.0 json-glib-1.0 libsoup-2.4 libpng
 DEPS=$(shell $(PREFIX)/env.sh pkg-config $(PKGCONFIG_ARGS) --libs --cflags $(LIBS))
 
 GNOME_SOURCES=http://ftp.gnome.org/pub/gnome/sources

bin/imgflo-runtime.c
@@ -2,6 +2,7 @@
 //     (c) 2014 The Grid
 //     imgflo may be freely distributed under the MIT license
 
+#include "lib/png.c"
 #include "lib/processor.c"
 #include "lib/graph.c"
 #include "lib/network.c"

imgflo.files
@@ -18,3 +18,4 @@ doc/braindump.md
 examples/demo.js
 examples/index.html
 examples/style.css
+lib/png.c

lib/graph.c
@@ -331,3 +331,21 @@ graph_load_stdin(Graph *self, GError **error) {
     return stdin_read && json_loaded;
 }
 
+const gchar *
+graph_find_processor_name(Graph *self, Processor *processor) {
+    g_return_val_if_fail(self, NULL);
+    g_return_val_if_fail(processor, NULL);
+
+    GHashTableIter iter;
+    gpointer key, value;
+    gchar *processor_name = NULL;
+
+    g_hash_table_iter_init(&iter, self->processor_map);
+    while (g_hash_table_iter_next(&iter, &key, &value))
+    {
+        if (value == processor) {
+            processor_name = (gchar *)key;
+        }
+    }
+    return processor_name;
+}

lib/png.c
@@ -1,0 +1,132 @@
+//     imgflo - Flowhub.io Image-processing runtime
+//     (c) 2014 The Grid
+//     imgflo may be freely distributed under the MIT license
+
+#include <png.h>
+#include <glib.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char *buffer;
+  size_t size;
+} PngEncoder;
+
+void
+write_data(png_structp png_ptr, png_bytep data, png_size_t length)
+{
+    PngEncoder* p = (PngEncoder *)png_get_io_ptr(png_ptr);
+    const size_t new_size = p->size + length;
+
+    if(p->buffer) {
+        p->buffer = g_realloc(p->buffer, new_size);
+    } else {
+        p->buffer = g_malloc(new_size);
+    }
+    if(!p->buffer) {
+        g_critical("%s: F", __PRETTY_FUNCTION__);
+        png_error(png_ptr, "Write Error");
+    }
+
+    memcpy(p->buffer + p->size, data, length);
+    p->size += length;
+}
+
+void
+flush_data(png_structp png_ptr)
+{
+}
+
+PngEncoder *
+png_encoder_new(void) {
+    PngEncoder *self = g_new(PngEncoder, 1);
+    self->buffer = NULL;
+    self->size = 0;
+    return self;
+}
+
+void
+png_encoder_free(PngEncoder *self) {
+    if (self->buffer) {
+        g_free(self->buffer);
+    }
+    g_free(self);
+}
+
+void
+png_encoder_encode_rgba(PngEncoder *self, int width, int height, gchar *buffer) {
+
+    png_byte color_type = PNG_COLOR_TYPE_RGBA;
+    png_byte bit_depth = 8;
+
+    png_bytep *row_pointers = row_pointers = (png_bytep *)g_malloc(sizeof(png_bytep) * height);
+    const size_t bytes_per_row = 1*4*width;
+
+    if (buffer) {
+        for(int y = 0; y < height; y++) {
+            const size_t offset = y*bytes_per_row;
+            row_pointers[y] = (png_byte*)(buffer + offset);
+        }
+    } else {
+        // test image
+        for(int y = 0; y < height; y++) {
+            row_pointers[y] = (png_byte*)g_malloc0(bytes_per_row);
+            for (int x=0; x < width; x++) {
+                const int off = x*4;
+                row_pointers[y][off+0] = 0xFF;
+                row_pointers[y][off+1] = 0x00;
+                row_pointers[y][off+2] = 0xFF;
+                row_pointers[y][off+3] = 0xAA;
+            }
+        }
+    }
+
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr)
+        g_critical("[write_png_file] png_create_write_struct failed");
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+        g_critical("[write_png_file] png_create_info_struct failed");
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+        g_critical("[write_png_file] Error during writing bytes");
+    png_set_rows(png_ptr, info_ptr, row_pointers);
+
+    //FILE *fp = fopen("pngtest2.png", "wb");
+    FILE *fp = NULL;
+    if (fp) {
+        png_init_io(png_ptr, fp);
+    } else {
+        png_set_write_fn(png_ptr, self, write_data, flush_data);
+    }
+    if (setjmp(png_jmpbuf(png_ptr)))
+        g_critical("[write_png_file] Error during init_io");
+
+    /* write header */
+    if (setjmp(png_jmpbuf(png_ptr)))
+        g_critical("[write_png_file] Error during writing header");
+    png_set_IHDR(png_ptr, info_ptr, width, height,
+                 bit_depth, color_type, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png_ptr, info_ptr);
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+        g_critical("[write_png_file] Error during writing bytes");
+    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    /* end write */
+    if (setjmp(png_jmpbuf(png_ptr)))
+        g_critical("[write_png_file] Error during end of write");
+    png_write_end(png_ptr, NULL);
+
+    /*
+    fp = fopen("pngtest2dir.png", "wb");
+    size_t written = fwrite(self->buffer, 1, self->size, fp);
+    if (written != self->size) {
+        g_printerr("%s, written=%d, size=%d\n",
+                   strerror(ferror(fp)), (int)written, (int)self->size);
+    }
+    if (fp) fclose(fp);
+    */
+}

lib/ui.c
@@ -285,11 +285,33 @@ ui_connection_handle_message(UiConnection *self,
     }
 }
 
+void
+send_preview_invalidated(Network *network, Processor *processor, GeglRectangle rect, gpointer user_data) {
+    SoupWebsocketConnection *conn = (SoupWebsocketConnection *)user_data;
+
+    const gchar *node = graph_find_processor_name(network->graph, processor);
+
+    // FIXME: do not hardcode url. Ideally relative URL should work, UI would make it relative to runtime
+    gchar url[1024];
+    g_snprintf(url, 1024, "http://localhost:3569/process?node=%s", node);
+
+    JsonObject *payload = json_object_new();
+    json_object_set_string_member(payload, "type", "previewurl");
+    json_object_set_string_member(payload, "url", url);
+    json_object_set_string_member(payload, "message", "preview invalidated!"); // TEMP
+    send_response(conn, "network", "output", payload);
+}
+
 static void
 on_web_socket_open(SoupWebsocketConnection *ws, gpointer user_data)
 {
 	gchar *url = soup_uri_to_string(soup_websocket_connection_get_uri (ws), FALSE);
 	g_print("WebSocket: client opened %s with %s\n", soup_websocket_connection_get_protocol(ws), url);
+
+    UiConnection *self = (UiConnection *)user_data;
+    self->network->on_processor_invalidated_data = (gpointer)ws;
+    self->network->on_processor_invalidated = send_preview_invalidated;
+
 	g_free(url);
 }
 
@@ -361,31 +383,74 @@ void websocket_callback(SoupServer *server,
 }
 
 static void
+process_image_callback (SoupServer *server, SoupMessage *msg,
+		 const char *path, GHashTable *query,
+		 SoupClientContext *context, gpointer user_data) {
+
+    UiConnection *self = (UiConnection *)user_data;
+    PngEncoder *encoder = png_encoder_new();
+
+    const gchar *node_name = g_hash_table_lookup(query, "node");
+
+    // TODO: move code into backend somewhere
+    Processor *processor = NULL;
+    if (self->graph && node_name) {
+        processor = g_hash_table_lookup(self->graph->processor_map, node_name);
+    }
+
+    if (processor) {
+        const Babl *format = babl_format("R'G'B'A u8");
+        // FIXME: take region-of-interest as parameter
+        GeglRectangle roi = gegl_node_get_bounding_box(processor->node);
+        // FIXME: take size as parameter, set scale to give approx that
+        const double scale = 1.0;
+        gchar *buffer = g_malloc(roi.width*roi.height*babl_format_get_bytes_per_pixel(format));
+        // XXX: maybe use GEGL_BLIT_DIRTY?
+        gegl_node_blit(processor->node, scale, &roi, format, buffer,
+                       GEGL_AUTO_ROWSTRIDE, GEGL_BLIT_DEFAULT);
+        png_encoder_encode_rgba(encoder, roi.width, roi.height, buffer);
+
+        g_free(buffer);
+    } else {
+        g_warning("Rendering fallback image");
+        png_encoder_encode_rgba(encoder, 100, 100, NULL);
+    }
+
+    const gchar *mime_type = "image/png";
+    char *buf = encoder->buffer;
+    const size_t len = encoder->size;
+
+    soup_message_set_status(msg, SOUP_STATUS_OK);
+    soup_message_set_response(msg, mime_type, SOUP_MEMORY_COPY, buf, len);
+
+    //png_encoder_free(encoder);
+}
+
+static void
 server_callback (SoupServer *server, SoupMessage *msg,
 		 const char *path, GHashTable *query,
 		 SoupClientContext *context, gpointer data)
 {
 
-	SoupMessageHeadersIter iter;
-	const char *name, *value;
+    SoupMessageHeadersIter iter;
+    const char *name, *value;
 
-	g_print("%s %s HTTP/1.%d\n", msg->method, path,
-		 soup_message_get_http_version(msg));
-	soup_message_headers_iter_init(&iter, msg->request_headers);
-	while (soup_message_headers_iter_next(&iter, &name, &value))
-		g_print("%s: %s\n", name, value);
-	if (msg->request_body->length)
-		g_print("%s\n", msg->request_body->data);
+    g_print("%s %s HTTP/1.%d\n", msg->method, path,
+         soup_message_get_http_version(msg));
 
-	char *file_path = g_strdup_printf(".%s", path);
-
-	if (msg->method == SOUP_METHOD_GET || msg->method == SOUP_METHOD_HEAD) {
-		g_print("GET REQUEST!!");
+    if (g_strcmp0(path, "/process") == 0 && msg->method == SOUP_METHOD_GET) {
+        process_image_callback(server, msg, path, query, context, data);
     } else {
-		soup_message_set_status(msg, SOUP_STATUS_NOT_IMPLEMENTED);
+        soup_message_headers_iter_init(&iter, msg->request_headers);
+        while (soup_message_headers_iter_next(&iter, &name, &value))
+            g_print("%s: %s\n", name, value);
+        if (msg->request_body->length)
+            g_print("%s\n", msg->request_body->data);
+
+        g_print("  -> %d %s\n\n", msg->status_code, msg->reason_phrase);
+
+        soup_message_set_status(msg, SOUP_STATUS_NOT_IMPLEMENTED);
     }
-	g_free(file_path);
-	g_print("  -> %d %s\n\n", msg->status_code, msg->reason_phrase);
 }
 
 

commit 38a78b887fd23c827fafab0b81d561fc4cc994e2

runtime: Emit invalidation regardless of reason, actually start processor
Preview now appears when starting the network, without having to change graph.
Also fixes breaking tests, regression since 84580eb09955f52d189de328fc1746f12a19661f
Refs #5

lib/network.c
@@ -49,9 +49,9 @@ network_set_graph(Network *self, Graph *graph)
 void 
 set_running_state_func(gpointer key, Processor *value, Network *network)
 {
-    processor_set_running(value, network->running);
     value->on_invalidated_data = (gpointer)network;
     value->on_invalidated = emit_invalidated;
+    processor_set_running(value, network->running);
 }
 
 void
@@ -60,6 +60,7 @@ network_set_running(Network *self, gboolean running)
     if (!self->graph) {
         return;
     }
+    self->running = running;
     g_hash_table_foreach(self->graph->processor_map, (GHFunc)set_running_state_func, self);
 }
 

lib/processor.c
@@ -49,6 +49,10 @@ trigger_processing(Processor *self, GeglRectangle roi)
 {
     g_return_if_fail(self->node);
 
+    if (self->on_invalidated) {
+        self->on_invalidated(self, roi, self->on_invalidated_data);
+    }
+
     if (!self->processor) {
         self->processor = gegl_node_new_processor(self->node, &roi);
         g_return_if_fail(self->processor);
@@ -79,9 +83,6 @@ invalidated_event(GeglNode *node, GeglRectangle *rect, Processor *self)
 {
     if (self->running) {
         trigger_processing(self, *rect);
-    }
-    if (self->on_invalidated) {
-        self->on_invalidated(self, *rect, self->on_invalidated_data);
     }
 }
 