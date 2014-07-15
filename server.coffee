imgflo/imgflo

commit f1b10ac67a282543d2f1e79d1bbc0aeae285ea2a

server: Fix failure on https:// protocol and bogus characters in extensions

server.coffee
@@ -253,11 +253,11 @@ class Server extends EventEmitter
 
         # Add extension so GEGL load op can use the correct file loader
         ext = path.extname src
-        if (ext.indexOf '&') != -1
+        if ext != '.png' or ext != '.jpg'
             ext = ''
 
         to = path.join @workdir, (hashFile src) + ext
-        if (src.indexOf 'http://') == -1
+        if (src.indexOf 'http://') == -1 and (src.indexOf 'https://') == -1
             src = 'http://localhost:'+@port+'/'+src
 
         downloadFile src, to, (err, contentType) =>

commit bbcbba10e8118c356cb57dbf72d9dd1697c9af5f

server: Fix failure on .png which GEGL could not sniff type of
Regression in extension logic introduced in f1b10ac,
but file is interesting as a test case regardless, as
normally GEGL should have understood it was a PNG from the content.

server.coffee
@@ -253,7 +253,7 @@ class Server extends EventEmitter
 
         # Add extension so GEGL load op can use the correct file loader
         ext = path.extname src
-        if ext != '.png' or ext != '.jpg'
+        if ext not in ['.png', '.jpg', '.jpeg']
             ext = ''
 
         to = path.join @workdir, (hashFile src) + ext

spec/graphtests.yml
@@ -24,3 +24,6 @@
 -
     _name: regcheck_colon_in_input_extension
     _url: '/graph/gradientmap?input=https%3A%2F%2Fs3.amazonaws.com%2Fksr%2Fassets%2F000%2F921%2F889%2F0f93b56c15adf6737b7f725162851e50_large.png%3F1379220875&color1=%23082A31&color2=%23FFE79B'
+-
+    _name: regcheck_unsniffable_png
+    _url: '/graph/gradientmap?input=http%3A%2F%2Febmedia.eventbrite.com%2Fs3-build%2Fimages%2F4783149%2F5973734463%2F1%2Flogo.png&color1=%23082A31&color2=%23FFE79B'