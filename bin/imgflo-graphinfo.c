7cfb3931e7d3343cf8cc30e47f4a27af1d32dcb0
Jon Nordby <jononor@gmail.com>
2016-08-31 16:17:45 +0200

    imgflo-graphinfo: Also accept input on stdin

--- bin/imgflo-graphinfo.c
+++ bin/imgflo-graphinfo.c
@@ -67,6 +67,34 @@ inject_exported_port_types(Graph *g, JsonObject *root) {
     return TRUE;
 }
 
+JsonObject *
+add_graphinfo(char *data, ssize_t length) {
+    GError *err = NULL;
+    Library *lib = library_new();
+    Graph *g = graph_new("default/main", lib);
+    gboolean loaded = graph_load_json_data(g, data, length, &err);
+    if (!loaded) {
+        g_printerr("Failed to load graph: %s", err->message);
+        return NULL;
+    }
+
+    // To ensure we preserve all metadata, we operate directly on the JSON,
+    // instead of potentially lossy roundtrip in Graph datastructure
+    JsonParser *parser = json_parser_new();
+    gboolean success = json_parser_load_from_data(parser, data, length, &err);
+    if (!success) {
+        g_printerr("Failed to load graph: %s", err->message);
+        return NULL;
+    }
+
+    JsonNode *rootnode = json_parser_get_root(parser);
+    g_assert(JSON_NODE_HOLDS_OBJECT(rootnode));
+    JsonObject *root = json_node_get_object(rootnode);
+
+    inject_exported_port_types(g, root);
+    return root;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -95,40 +123,48 @@ main (int argc, char **argv)
 
         gegl_init(0, NULL);
 
-        if (strlen(graphfile) > 0) {
-            GError *err = NULL;
-            Library *lib = library_new();
-            Graph *g = graph_new("default/main", lib);
-            //Network *n = network_new(g);
-            gboolean loaded = graph_load_json_file(g, graphfile, &err);
-            if (!loaded) {
-                g_printerr("Failed to load graph: %s", err->message);
-                return 1;
-            }
-
-            // To ensure we preserve all metadata, we operate directly on the JSON,
-            // instead of potentially lossy roundtrip in Graph datastructure
-            JsonParser *parser = json_parser_new();
-            gboolean success = json_parser_load_from_file(parser, graphfile, &err);
-            if (!success) {
-                g_printerr("Failed to load graph: %s", err->message);
-                return 1;
-            }
-
-            JsonNode *rootnode = json_parser_get_root(parser);
-            g_assert(JSON_NODE_HOLDS_OBJECT(rootnode));
-            JsonObject *root = json_node_get_object(rootnode);
-
-            inject_exported_port_types(g, root);
-            gchar *output = json_stringify(root, NULL);
-
-            g_print("%s", output);
-            g_free(output);
-
-        } else {
+        gchar *input_data = NULL;
+        gsize input_data_length = 0;
+        if (strlen(graphfile) == 0) {
             g_printerr("No --graph specified");
             return 1;
+        } else if (g_strcmp0(graphfile, "-") == 0) {
+            // stdint
+            size_t max_bytes = 1e6;
+            void * buffer = g_malloc(max_bytes);
+            size_t bytes_read = 0;
+            GInputStream *stdin_stream = g_unix_input_stream_new(STDIN_FILENO, FALSE);
+            GError *error = NULL;
+            gboolean stdin_read = g_input_stream_read_all(stdin_stream, buffer, max_bytes, &bytes_read, NULL, &error);
+            if (!stdin_read) {
+                return 1;
+            }
+            input_data = buffer;
+            input_data_length = bytes_read;
+        } else {
+            // file path
+            GFile *file = g_file_new_for_path(graphfile);
+            GError *error = NULL;
+            gboolean file_read = g_file_load_contents(file, NULL, &input_data, &input_data_length, NULL, &error);
+            g_object_unref(file);
+            if (!file_read) {
+                return 1;
+            }
         }
+
+        g_assert(input_data);
+
+        JsonObject *out = add_graphinfo(input_data, input_data_length);
+        g_free(input_data); input_data_length = 0;
+        if (!out) {
+            return 1;
+        }
+
+        gchar *output = json_stringify(out, NULL);
+        g_print("%s", output);
+        g_free(output);
+
+
 
         gegl_exit();
     }
